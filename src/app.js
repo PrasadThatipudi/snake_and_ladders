68e9fc31a918021298f4d1301a172fdccfc717fb
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-03-29 15:10:17 +0530

    adds logRequest to maintain log for request records

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-const handler = () => {};
+const insertLogRecord = async (logRecord) =>
+  await Deno.writeTextFile("./logRequest.txt", logRecord, { append: true });
+
+const logRequest = (request) => {
+  const date = new Date();
+  const timeString = date.toTimeString().split(" ").at(0);
+  const dateString = date.toDateString();
+  const userAgent = request.headers.get("user-agent");
+
+  const logRecord = [
+    dateString,
+    timeString,
+    request.method,
+    request.url,
+    userAgent,
+  ]
+    .join(" ")
+    .concat("\n");
+
+  insertLogRecord(logRecord);
+  console.log(logRecord);
+};
+
+const handler = (request) => {
+  logRequest(request);
+
+  return new Response("ok");
+};
 
 export { handler };
